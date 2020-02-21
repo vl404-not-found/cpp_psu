4a60c4bd1598d547259f3a6351b440c0580d1c6d
Влад Шкердин <shkerdin.v@ya.ru>
2020-02-21 15:43:10 +0300

    cm

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -17,7 +17,9 @@ std::string lab2() {
     array_fill(main_array, n);
 
     form(main_array, n, slave_array);
-
     array_print(slave_array, n);
+    int first_result = mean(main_array, n);
+    int second_result = mean(slave_array, n);
+    std::cout << first_result << "\n" << second_result;
     return "Ok";
 }
---
+++ lab2/mean.cpp
@@ -0,0 +1,10 @@
+//
+// Created by vlad on 21.02.2020.
+//
+
+int mean (int *a,  int n){
+    int result = 0;
+    for (int i = 0; i < n; i++)
+        result += a[i];
+    return result/n;
+}

9cb303d5b0c635333cf7409eae8e3e7671e4ca0c
Влад Шкердин <shkerdin.v@ya.ru>
2020-09-05 21:45:07 +0300

    add class in lab 2 for work with books

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
+#include <term_3/lab_2/lab_2.h>
 #include <iostream>
-#include "term_3/lab_1/lab_1.h"
+
+using namespace std;
 
 int main() {
-    setlocale(LC_ALL,"Russian");
-    lab_1();
+    setlocale(LC_ALL, "Russian");
+    auto * books = new vector<Book>;
+    try {
+        Book *mew = Book::createBook();
+    } catch (const char* exception) {
+        cout<<exception;
+    }
+
     return 0;
 }
 
--- term_3/lab_2/lab_2.h
+++ term_3/lab_2/lab_2.h
@@ -5,9 +5,28 @@
 #ifndef CPP_PSU_LAB_2_H
 #define CPP_PSU_LAB_2_H
 
+#include <string>
+#include <vector>
 
-class lab_2 {
+using namespace std;
 
+class Book {
+    string author;
+    string name;
+    string pub;
+    int year;
+    int nop;
+
+    explicit Book(string, string, string, int, int);
+
+public:
+    static Book *createBook();
+
+    static void findByAuthor(vector<Book>, const string &);
+
+    static void findByPub(vector<Book>, const string &);
+
+    static void findIfMoreThenYear(vector<Book>, int);
 };
 
 

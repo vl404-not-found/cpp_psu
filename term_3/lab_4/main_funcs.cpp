e13d0db7359868278fc219e71c02836b402013b9
Влад Шкердин <shkerdin.v@ya.ru>
2020-09-26 08:52:30 +0300

    remove unused code and add getters and setters

--- term_3/lab_4/main_funcs.cpp
+++ term_3/lab_4/main_funcs.cpp
@@ -11,10 +11,12 @@ void main_lab() {
     auto listEmp = new vector<Employee>;
     bool flag = true;
 
-    float res = 0;
-    for (int i = 0; i <= 2; i++)
-        res = sqrt(float(i) + sqrt(res));
-    std::cout << res;
+//    float res = 0;
+//    for (int i = 0; i <= 2; i++)
+//        res = sqrt(float(i) + sqrt(res));
+//    cout << res;
+
+
 //    while (flag){
 //        Employee item = Employee();
 //        listEmp->push_back(item);
